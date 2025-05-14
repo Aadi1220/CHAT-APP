ea17daedba8d23e7cbbc6b57326bf5d774e04f23
Aadi1220 <aadityaattri262@gmail.com>
2025-05-14 22:21:56 +0530

    size issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,9 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+// Increase payload size limit
+app.use(express.json({ limit: "50mb" })); // Increase JSON payload limit
+app.use(express.urlencoded({ limit: "50mb", extended: true })); // Increase URL-encoded payload limit
 app.use(cookieParser());
 app.use(
   cors({
